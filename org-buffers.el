99a811fd8869e4846c6f87b45c76cf710b102dcb
Dan Davison <davison@stats.ox.ac.uk>
2010-03-29 19:21:14 -0400

    New function org-buffers-merge-params

--- org-buffers.el
+++ org-buffers.el
@@ -209,3 +209,16 @@ The heading is a link to `buffer'."
 
 (provide 'org-buffers)
 ;;; org-buffers.el ends here
+(defun org-buffers-merge-params (&rest plists)
+  "Merge parameters.
+Earlier values have precedence over later ones."
+  (let (params)
+    (mapc
+     (lambda (plist)
+       (mapc (lambda (pair)
+	       (unless (assoc (car pair) params)
+		 (add-to-list 'params pair)))
+	     plist))
+     plists)
+    params))
+
